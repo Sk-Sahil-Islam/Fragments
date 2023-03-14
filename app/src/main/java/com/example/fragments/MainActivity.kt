dbf4ca1ed7a6f50a69db71803547e3a08f17fb7d
Sk-Sahil-Islam <sksahilislam0123@gmail.com>
2023-03-14 16:32:58 +0530

    Final

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fragmentbtn1.setOnClickListener {
+
+            replaceFragment(Fragment1())
+
+        }
+
+        binding.fragmentbtn2.setOnClickListener {
+
+            replaceFragment(Fragment2())
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
